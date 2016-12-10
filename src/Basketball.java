15864c6483e0c670b81b78e23cdbbff60a6c2106
Aaron Mueller <ammaaronmueller@gmail.com>
2016-12-10 14:25:32 -0500

    Added Basketball class file

---
+++ src/Basketball.java
@@ -0,0 +1,12 @@
+import com.jogamp.opengl.GL2;
+
+public class Basketball {
+	private float x_pos;
+	private float y_pos;
+	private float z_pos;
+	
+	protected TextureLoader texture_loader = null;
+	private String skybox_name = null;
+	
+	
+}
