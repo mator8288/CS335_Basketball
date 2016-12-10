5244cfa760b6ddb06fb5b2f43a4f9b30627438ab
Aaron Mueller <ammaaronmueller@gmail.com>
2016-12-10 14:05:49 -0500

    Added Comments

--- src/main.java
+++ src/main.java
@@ -1,3 +1,10 @@
+/* Names: Aaron Mueller; Thomas Jones
+ * Course: CS335
+ * Date: 12 December 2016
+ * Assignment: Final Project
+ * Purpose: To run the project
+ */
+
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.awt.GLCanvas;
 import javax.swing.JFrame;
@@ -15,7 +22,6 @@ public class main extends JFrame {
 		
 		setupJOGL();
 	}
-	//comment
 	
 	public static void main( String[] args ) {
 		main m = new main();
